13bb35e51e616bd694df5c3066c26f1252af2a77
YiCheng Luo <jojoluo0810@gmail.com>
2023-04-15 16:13:31 +0800

    add header

--- product.py
+++ product.py
@@ -4,15 +4,15 @@ while True: #用while通常是在不知道執行次數的條件下使用
 	if name == 'q': #quit
 		break
 	price = input('請輸入價格： ')	
+	price = int(price) #型別轉換成整數
 	products.append([name, price])
 print(products)
 
-#拿出大清單products裡面的資料
-for p in products:
-	print(p)
-#拿出內層清單的資料
-for p in products:
-	print(p[0])
-#組合使用
-for p in products:
+for p in products: 
 	print(p[0], '的價格是', p[1])
+
+
+with open ('products.csv', 'w', encoding='utf-8') as f: #編碼調整
+	f.write('商品,價格\n') #增加欄位
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n') #將price變為字串
