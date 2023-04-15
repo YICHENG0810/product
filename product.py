78c97628756fb3e6c34df1f48dd46f86abd9e4c3
YiCheng Luo <jojoluo0810@gmail.com>
2023-04-15 13:43:11 +0800

    add for loop for print product

--- product.py
+++ product.py
@@ -6,3 +6,13 @@ while True: #用while通常是在不知道執行次數的條件下使用
 	price = input('請輸入價格： ')	
 	products.append([name, price])
 print(products)
+
+#拿出大清單products裡面的資料
+for p in products:
+	print(p)
+#拿出內層清單的資料
+for p in products:
+	print(p[0])
+#組合使用
+for p in products:
+	print(p[0], '的價格是', p[1])
